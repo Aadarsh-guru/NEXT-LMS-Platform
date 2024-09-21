eaf6ad779045c42237ab17b7cf74f54842fe342a
Aadarsh Guru <112774513+Aadarsh-guru@users.noreply.github.com>
2024-09-21 09:07:12 +0000

    Fixed: Course page login error

--- src/components/courses/CourseEnrollButton.tsx
+++ src/components/courses/CourseEnrollButton.tsx
@@ -1,11 +1,21 @@
 "use client";
-import { useSession } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
 import { useEffect, useState } from 'react';
 import { Loader2 } from 'lucide-react';
 import { Button } from '../ui/button';
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+} from "@/components/ui/dialog";
 import { checkoutAction, verifyPaymentAction } from '@/actions/checkout';
 import { toast } from '../ui/use-toast';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
+import Image from 'next/image';
+import { cn } from '@/lib/utils';
 
 interface CourseEnrollButtonProps {
     price: number;
@@ -15,8 +25,13 @@ interface CourseEnrollButtonProps {
 
 const CourseEnrollButton: React.FC<CourseEnrollButtonProps> = ({ price, courseId, children }) => {
 
+    const [openDialog, setOpenDialog] = useState<boolean>(false);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
     const session = useSession();
     const router = useRouter();
+    const serachParams = useSearchParams();
+
     const [isVeryfying, setIsVeryfying] = useState<boolean>(false);
     const [loading, setLoading] = useState<boolean>(false);
 
@@ -75,6 +90,7 @@ const CourseEnrollButton: React.FC<CourseEnrollButtonProps> = ({ price, courseId
         try {
             const { success, order } = await checkoutAction(price);
             if (!success) {
+                setOpenDialog(true);
                 return toast({
                     title: "Something went wrong.",
                     variant: "destructive",
@@ -115,25 +131,81 @@ const CourseEnrollButton: React.FC<CourseEnrollButtonProps> = ({ price, courseId
         }
     };
 
+    const onSignIn = async (method: 'google') => {
+        setIsLoading(true);
+        try {
+            await signIn(method, {
+                redirect: false
+            });
+            setOpenDialog(false);
+        } catch (error) {
+            console.log(error);
+            toast({
+                title: serachParams.get('error') || 'somethiong went wrong.',
+                variant: "destructive",
+            });
+        } finally {
+            setIsLoading(false);
+        }
+    };
 
     return (
-        <Button
-            type="button"
-            onClick={handleCheckout}
-            disabled={loading || isVeryfying}
-        >
-            {(loading || isVeryfying) ? (
-                <>
-                    <Loader2 className="h-4 w-4 animate-spin mr-2" />
-                    {loading && 'Processing..'}
-                    {isVeryfying && 'Verifying..'}
-                </>
-            ) : (
-                <>
-                    {children}
-                </>
-            )}
-        </Button>
+        <>
+            <Button
+                type="button"
+                onClick={handleCheckout}
+                disabled={loading || isVeryfying}
+            >
+                {(loading || isVeryfying) ? (
+                    <>
+                        <Loader2 className="h-4 w-4 animate-spin mr-2" />
+                        {loading && 'Processing..'}
+                        {isVeryfying && 'Verifying..'}
+                    </>
+                ) : (
+                    <>
+                        {children}
+                    </>
+                )}
+            </Button>
+            <Dialog open={openDialog} onOpenChange={() => setOpenDialog(false)} >
+                <DialogContent className="max-w-sm" >
+                    <DialogHeader>
+                        <DialogTitle className="text-center" >Sign in</DialogTitle>
+                        <DialogDescription className="text-center" >
+                            To continue to the platform.
+                        </DialogDescription>
+                    </DialogHeader>
+                    <DialogFooter className="py-4" >
+                        <Button
+                            onClick={() => onSignIn('google')}
+                            className={cn(
+                                "w-full flex items-center justify-center gap-x-2",
+                                isLoading && "cursor-not-allowed"
+                            )}
+                            variant={'outline'}
+                            disabled={isLoading}
+                        >
+                            {isLoading ? (
+                                <Loader2 className="h-5 w-5 animate-spin" />
+                            ) : (
+                                <Image
+                                    src="/google.svg"
+                                    alt="Google logo"
+                                    width={20}
+                                    height={20}
+                                    className="mr-2 h-5 w-5"
+                                />
+                            )}
+                            Continue with google
+                        </Button>
+                    </DialogFooter>
+                    <div className="text-xs text-gray-700 text-center">
+                        By creating an account, you agree to our Terms of Service and Privacy Policy.
+                    </div>
+                </DialogContent>
+            </Dialog>
+        </>
     );
 };
 
